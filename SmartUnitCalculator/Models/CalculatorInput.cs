f9dffd2d565b3d52e5e4d086a00e36ee5c4f1760
Wiktor <wiktor.szczeszek99.other@gmail.com>
2022-07-16 22:45:10 +0200

    Przeniesienie Vm do Index

---
+++ SmartUnitCalculator/Models/CalculatorInput.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartUnitCalculator.Models
+{
+    public class CalculatorInput
+    {
+        public int BaseUnitId { get; set; }
+        public int ResultUnitId { get; set; }
+        public decimal BaseValue { get; set; }
+    }
+}
