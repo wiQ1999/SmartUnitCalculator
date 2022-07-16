839557bb9f7fb00780c0dc4933275ecfeefabb92
Wiktor <wiktor.szczeszek99.other@gmail.com>
2022-07-16 11:46:41 +0200

    Zmiana rozwiązania na RazorPages MVVM

--- SmartUnitCalculator/Program.cs
+++ SmartUnitCalculator/Program.cs
@@ -4,7 +4,7 @@ using SmartUnitCalculator.Database;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SUCDB_W")
     ?? throw new InvalidOperationException("Connection string not found.")));
@@ -14,7 +14,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -26,8 +26,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
