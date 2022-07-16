698277eb24f4314f577e10fe34d810f2dafb785d
FairyDonut <julianna.gorska99@gmail.com>
2022-07-16 23:08:38 +0200

    add user page

---
+++ SmartUnitCalculator/Pages/Shared/User.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SmartUnitCalculator.Pages.Shared
+{
+    public class UserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
